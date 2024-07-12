58d5b8a979f9509c51012cc094d219fb1395af98
skillzee <parthsharmaisnumber1@gmail.com>
2024-07-12 15:40:44 +0530

    porject setup

--- app/index.jsx
+++ app/index.jsx
@@ -5,9 +5,9 @@ import { Link } from 'expo-router';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className=" text-3xl">Aora</Text>
+      <Text className=" text-3xl font-pblack">Aora</Text>
       <StatusBar style="auto" />
-      <Link href="/profile">Click Here</Link>
+      <Link href="/profile" className=' text-blue-200'>Click Here</Link>
     </View>
   );
 }
